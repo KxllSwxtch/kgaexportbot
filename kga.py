0df5567774ea8e9819064f8132d0e3f83cb91ba6
KxllSwxtch <dimashin9@gmail.com>
2024-11-19 09:32:33 +0900

    updates

--- kga.py
+++ kga.py
@@ -309,9 +309,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left
         try:
-            product_left = WebDriverWait(driver, 8).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "product_left"))
-            )
+            product_left = driver.find_element(By.CLASS_NAME, "product_left")
             product_left_splitted = product_left.text.split("\n")
 
             car_title = product_left.find_element(
@@ -349,9 +347,7 @@ def get_car_info(url):
 
         # Проверка элемента gallery_photo
         try:
-            gallery_element = WebDriverWait(driver, 7).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div.gallery_photo"))
-            )
+            gallery_element = driver.find_element(By.CSS_SELECTOR, "div.gallery_photo")
             car_title = gallery_element.find_element(By.CLASS_NAME, "prod_name").text
             items = gallery_element.find_elements(By.XPATH, ".//*")
 
