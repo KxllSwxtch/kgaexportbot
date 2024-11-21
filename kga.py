0cefed41db98ce28df8928592e64945e41223755
KxllSwxtch <dimashin9@gmail.com>
2024-11-21 22:17:58 +0900

    updates

--- kga.py
+++ kga.py
@@ -308,7 +308,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left
         try:
-            product_left = WebDriverWait(driver, 8).until(
+            product_left = WebDriverWait(driver, 6).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "product_left"))
             )
             product_left_splitted = product_left.text.split("\n")
