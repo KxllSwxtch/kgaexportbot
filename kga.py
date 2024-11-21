ee07009873828cec20bd072dbb2d4e0256120b9b
KxllSwxtch <dimashin9@gmail.com>
2024-11-21 22:28:05 +0900

    updates

--- kga.py
+++ kga.py
@@ -408,6 +408,7 @@ def get_car_info(url):
     finally:
         # Обработка всплывающих окон (alerts)
         try:
+            WebDriverWait(driver, 3).until(EC.alert_is_present())
             alert = driver.switch_to.alert
             alert.dismiss()
             logging.info("Всплывающее окно отклонено.")
