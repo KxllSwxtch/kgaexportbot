b0d0187ba8f8273d07c424c3190ac8f273716c6c
KxllSwxtch <dimashin9@gmail.com>
2024-11-22 10:22:19 +0900

    updates

--- kga.py
+++ kga.py
@@ -234,7 +234,7 @@ def load_cookies(driver):
 
 def check_and_handle_alert(driver):
     try:
-        WebDriverWait(driver, 4).until(EC.alert_is_present())
+        # WebDriverWait(driver, 4).until(EC.alert_is_present())
         alert = driver.switch_to.alert
         print(f"Обнаружено всплывающее окно: {alert.text}")
         alert.accept()  # Закрывает alert
