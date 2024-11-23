64320fb3dda56989402e0823cc7ff71b0f35666a
KxllSwxtch <dimashin9@gmail.com>
2024-11-23 10:45:45 +0900

    updates

--- kga.py
+++ kga.py
@@ -270,6 +270,7 @@ def get_car_info(url):
     try:
         # Загружаем страницу
         driver.get(url)
+        time.sleep(5)
         check_and_handle_alert(driver)
         load_cookies(driver)
 
@@ -308,9 +309,7 @@ def get_car_info(url):
 
         # Проверка элемента product_left
         try:
-            product_left = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "product_left"))
-            )
+            product_left = driver.find_element(By.CLASS_NAME, "product_left")
             product_left_splitted = product_left.text.split("\n")
 
             car_title = product_left.find_element(
