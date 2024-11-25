21f0618c4322780fad698602f0426de479f30023
KxllSwxtch <dimashin9@gmail.com>
2024-11-25 11:28:57 +0900

    updates

--- kga.py
+++ kga.py
@@ -37,7 +37,7 @@ https_proxy = f"https://{PROXY_USER}:{PROXY_PASS}@{PROXY_IP}:{PROXY_PORT}"
 
 proxy = {
     "http": f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_IP}:{PROXY_PORT}",
-    "https": f"https://{PROXY_USER}:{PROXY_PASS}@{PROXY_IP}:{PROXY_PORT}",
+    "https": f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_IP}:{PROXY_PORT}",
 }
 
 session = requests.Session()
