9292f396703c85e0cf39547229196348e683b812
KxllSwxtch <dimashin9@gmail.com>
2024-12-06 14:49:49 +0900

    updates

--- kga.py
+++ kga.py
@@ -304,7 +304,7 @@ def send_recaptcha_token(token):
 def create_driver():
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     # chrome_options.add_argument("--start-maximized")
